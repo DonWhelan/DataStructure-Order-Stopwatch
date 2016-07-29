b5199821f1626e854db23255635139b45f252dc4
DonWhelan <donwhelan1612@gmail.com>
2016-07-29 17:37:31 +0100

    First Commit- Array list add and search stopwatch

---
+++ src/datastruturestopwatch/ArrayDS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastruturestopwatch;
+
+/**
+ *
+ * @author donwhelan1
+ */
+public class ArrayDS {
+    
+    private int id; 
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toString(){
+        return "Id: "+id;
+    }
+    
+}
---
+++ src/datastruturestopwatch/ArrayListDS.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastruturestopwatch;
+
+/**
+ *
+ * @author donwhelan1
+ */
+public class ArrayListDS {
+
+    
+    
+    private int id; 
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toString(){
+        return "Id: "+id;
+    }
+    
+}
